2d2b10c84c1393e1a6b9f9c3fe7ec2227f4dba4b
Jean-Claude Ndagijimana <jeclauded@gmail.com>
2020-10-24 22:11:12 +1030

    Coginto Setup is done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.min.css';
 import './index.css';
 import App from './App';
+import Amplify from 'aws-amplify';
+import config from './config';
 import * as serviceWorker from './serviceWorker';
+
+// "invokeUrl": "https://5bltcq602h.execute-api.us-west-2.amazonaws.com/prod"
+
+
+Amplify.configure({
+    Auth: {
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
