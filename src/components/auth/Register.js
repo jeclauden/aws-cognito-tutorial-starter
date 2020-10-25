3f8b0f1b919b3fb485d87567752574a3130aba4a
Jean-Claude Ndagijimana <jeclauded@gmail.com>
2020-10-25 11:21:26 +1030

    Update Register.js

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormErrors from "../FormErrors";
 import Validate from "../utility/FormValidation";
+import { Auth } from "aws-amplify";
 
 class Register extends Component {
   state = {
